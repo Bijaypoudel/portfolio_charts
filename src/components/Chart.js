f77b85b16d2c04631ab65f21883e27f2a8623640
Bijay Poudel <bijay.r.poudel@gmail.com>
2021-01-19 01:48:04 -0600

    Initial Commit

---
+++ src/components/Chart.js
@@ -0,0 +1,23 @@
+import React, {useEffect, useState} from 'react';
+import {Bar, Line, Pie} from 'react-chartjs-2';
+
+
+const Chart = () => {
+
+    
+    // const [chart_data, set_Data] = useState({
+
+    // });
+
+
+    return (
+        <div className='chart'>
+            <Bar
+                
+            />
+
+        </div>
+        )
+}
+ 
+export default Chart;
